e24ac56872f9f7fb10a47e77e7b718cd769552ac
Soampbar <Althories.bar@gmail.com>
2025-05-16 12:48:31 -0500

    -updated collision label
    yeah

--- Scripts/collision_label.gd
+++ Scripts/collision_label.gd
@@ -3,7 +3,7 @@ extends Label
 #I don't know what's causing it but the signal fires once on ready() per obstacle in scene.
 #The negative count here should reflect the number of obstacle nodes in the scene
 
-var collision_count = -2
+var collision_count = -61
 
 func _ready():
 	text = "Collision Count: %s" % collision_count
